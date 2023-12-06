f36a95783cfa1830306addb7904af8241c75a6be
Allan Rolle <allan.rolle@laposte.net>
2023-12-06 15:25:26 +0100

    card component

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  @Input() public name!:string;
+  @Input() public isEmployed!:boolean;
+  @Input() public avatarURL!:string;
+}
