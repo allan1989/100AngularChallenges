ee748c1e0998be02e48e7a98249a99c6a2f14009
Allan Rolle <allan.rolle@laposte.net>
2023-12-07 15:03:53 +0100

    progress bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { AccordionComponent } from './components/accordion/accordion.component';
             RouterOutlet, 
             ToolbarComponent, 
             CardComponent,
+            ProgressBarComponent,
             AccordionComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
